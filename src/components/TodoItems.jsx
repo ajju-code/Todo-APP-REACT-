e82148dc36cb608466dba6dc25937e0a5bfce145
ajjucode <dawreasjad72@gmail.com>
2024-01-17 10:23:14 +0530

    Your commit message

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -6,6 +6,7 @@ const TodoItems = ({ todoItems, onDeleteclick }) => {
       <div className={sty.itemsContainer}>
         {todoItems.map((items) => (
           <TodoItem
+            key={items.name}
             todoName={items.name}
             tododate={items.due_date}
             onDeleteclick={onDeleteclick}
